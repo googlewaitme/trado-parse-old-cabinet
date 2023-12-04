e39b08e2a48d11a44dc7f9354eb229ba51f6217f
bulat <kusok.apelbsina@hotmail.com>
2023-12-04 16:09:32 +0300

    need to build last step

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ from bs4 import BeautifulSoup
 from models import Order, Contacts, Position
 import config
 from create_session import create_session
+from db import DBConnector
+from b24 import OrderFactory
 
 
 def parse_contacts(page: BeautifulSoup) -> Contacts:
@@ -115,26 +117,25 @@ def main():
 
     page = BeautifulSoup(parsedata.text, "html.parser")
 
-    #  TODO last_loaded_id from database
-    '''
     db = DBConnector(
         config.DB_HOST, config.DB_NAME, config.DB_USER, config.DB_PASSWORD
     )
-    db.save("some_value", "some_result2")
-    print(db.get("some_value"))
-    '''
-    #  TODO check is last
-    #  TODO upload on bitrix 24
-    '''
-    order_factory = OrderFactory(config.B24_API_TOKEN)
-    order_factory.create_order(order)
-    '''
-    orders = parse_orders_list(page)
-    print(orders)
+    last_uploaded_id = 1
+    if db.is_exists("LAST_UPLOADED_ID"):
+        last_uploaded_id = int(db.get("LAST_UPLOADED_ID"))
+    else:
+        db.save("LAST_UPLOADED_ID", str(last_uploaded_id))
 
-    parse_data = session.get(config.BASE_URL + "/node/24452709/edit")
-    page = BeautifulSoup(parse_data.text, "html.parser")
-    print(parse_contacts(page))
+    orders = parse_orders_list(page)
+    # TODO filter orderds which ID > last_uploaded_id
+    print(orders)
+    for order in orders:
+        # TODO add contacts here
+        parse_data = session.get(config.BASE_URL + "/node/24452709/edit")
+        page = BeautifulSoup(parse_data.text, "html.parser")
+        print(parse_contacts(page))
+        order_factory = OrderFactory(config.B24_API_TOKEN)
+        order_factory.create_order(order)
 
 
 if __name__ == "__main__":
